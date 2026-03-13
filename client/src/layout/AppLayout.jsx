ade870368222fa28cdb463b957e6cbd8a70453e0
Catherine Jones <103010935+ozzycat@users.noreply.github.com>
2026-03-12 22:05:18 -0500

    prepping login (again)

--- client/src/layout/AppLayout.jsx
+++ client/src/layout/AppLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Container from "@mui/material/Container";
 
 export default function AppLayout() {
     return (
         <div className="app-container">
             <div className="app-body">
                 <main className="app-content">
-                    
-                    <Outlet/>
+                    <Container>
+                        <Outlet/>
+                    </Container>
                 </main>
             </div>
         </div>
--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,7 +1,49 @@
+import Container from "@mui/material/Container";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
 export default function Login() {
     return (
         <>
-            testing login
+            <Container>
+                <Card variant="outlined">
+                    <CardContent>
+                        <h1>Sign In</h1>
+                        <Box component="form" noValidate>
+                            <FormControl>
+                                <FormLabel htmlFor="email">Email</FormLabel>
+                                <TextField
+                                    id="email"
+                                    type="email"
+                                    name="email"
+                                    placeholder="your@email.com"
+                                    autoFocus
+                                    required
+                                    variant="outlined"
+                                />
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel htmlFor="password">Password</FormLabel>
+                                <TextField
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    placeholder="••••••"
+                                    autoFocus
+                                    required
+                                    variant="outlined"
+                                />
+                            </FormControl>
+                            <Button type="submit" variant="contained">Sign In</Button>
+                        </Box>
+                    </CardContent>
+                </Card>
+            </Container>
         </>
     );
 }
