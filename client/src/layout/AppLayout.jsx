90789e58040458858ee05faa5eedb18946353af6
Catherine Jones <103010935+ozzycat@users.noreply.github.com>
2026-03-12 23:13:54 -0500

    add mui datagrid
    implement placeholder data grid in preparation for setting up the table containing the car line

--- client/src/layout/AppLayout.jsx
+++ client/src/layout/AppLayout.jsx
@@ -1,14 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Container from "@mui/material/Container";
+import { Paper } from "@mui/material";
 
 export default function AppLayout() {
     return (
         <div className="app-container">
             <div className="app-body">
                 <main className="app-content">
-                    <Container>
                         <Outlet/>
-                    </Container>
                 </main>
             </div>
         </div>
--- client/src/screens/Dashboard.jsx
+++ client/src/screens/Dashboard.jsx
@@ -1,7 +1,26 @@
+import Paper from "@mui/material/Paper";
+import { DataGrid } from '@mui/x-data-grid';
+import Box from "@mui/material/Box";
+
 export default function Dashboard() {
+
+    const columns = [
+        { field: 'name', headerName: 'Product Name', width: 200 },
+        { field: 'description', headerName: 'Description', width: 300 },
+    ];
+
+    const rows = [
+        { id: 1, name: 'Product A', description: 'Description of Product A' },
+        { id: 2, name: 'Product B', description: 'Description of Product B' },
+    ];
+
     return (
         <>
-            testing dashboard
+            <Paper>
+                <Box sx={{ height: 400, width: '100%' }}>
+                    <DataGrid columns={columns} rows={rows} />
+                </Box>      
+            </Paper>
         </>
     );
 }
