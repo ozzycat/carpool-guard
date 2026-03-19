9ae31398ec5b72da43796e1ee1e966aec8c58eb8
Catherine Jones <103010935+ozzycat@users.noreply.github.com>
2026-03-18 20:39:39 -0500

    installing toolpad to help secure authentication

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { AppProvider } from '@toolpad/core/AppProvider';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </StrictMode>,
 )
