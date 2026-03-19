1f6016c938508e8ea7d9b940a783d8a95c5804d6
Catherine Jones <103010935+ozzycat@users.noreply.github.com>
2026-03-18 20:41:17 -0500

    Revert "installing toolpad to help secure authentication"
    This reverts commit 9ae31398ec5b72da43796e1ee1e966aec8c58eb8.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { AppProvider } from '@toolpad/core/AppProvider';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <App />
   </StrictMode>,
 )
